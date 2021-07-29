7a65cc045176cd873f8e8d31dc537ac47310d273
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-29 08:51:25 -0300

    [IMP] storage_image_product: technical improvements

--- storage_image_product/models/product_template.py
+++ storage_image_product/models/product_template.py
@@ -2,12 +2,7 @@
 # @author Raphaël Reverdy <https://github.com/hparfr>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
-import logging
-
 from odoo import fields, models
-
-_logger = logging.getLogger(__name__)
 
 
 class ProductTemplate(models.Model):
